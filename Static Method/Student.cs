39b2682d233df41ee58c8d63ae9d3f5e6e1a45d1
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-15 22:48:25 +0700

    Initial commit

---
+++ Static Method/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Static_Method
+{
+    internal class Student
+    {
+        private int rollno;
+        private string name;
+        private static string college = "BBDIT";
+
+        //constructor to initialize the variable
+        public Student(int r, string n)
+        {
+            rollno = r;
+            name = n;
+        }
+
+        //static method to change the value of static variable
+
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+
+        //method to display values
+        public void Display()
+        {
+            Console.WriteLine(rollno + " " + name + " " + college);
+        }
+    }
+}
